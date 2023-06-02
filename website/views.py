a40dc85cd29bdfea1dc886bf883a921f84d8befa
rsrzk <ahmadraisrazak@gmail.com>
2023-06-02 10:31:55 +0800

    Added logo

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, flash, request, jsonify, session
 from flask_login import login_required, current_user
 from .models import Note, Company
 from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -35,15 +36,15 @@ def home():
             return render_template("home.html", user=current_user, no_company=True)
 
 @views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
-            
+
     return jsonify({})
 
 @views.route('/demo')
